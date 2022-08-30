1d08f439474047efb061797de1a2d55c922fca8c
Tankica Koteva <koteva.tankica@gmail.com>
2022-08-30 17:10:21 +0200

    car list

--- core/src/main/java/com/carproject/core/models/CarFilterModel.java
+++ core/src/main/java/com/carproject/core/models/CarFilterModel.java
@@ -1,6 +1,6 @@
 package com.carproject.core.models;
-
 import org.apache.sling.api.resource.Resource;
+
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ResourcePath;
--- core/src/main/java/com/carproject/core/models/CarModel.java
+++ core/src/main/java/com/carproject/core/models/CarModel.java
@@ -1,9 +1,10 @@
 package com.carproject.core.models;
 
-import jdk.internal.loader.Resource;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
 
 @Model(adaptables = Resource.class)
 public class CarModel {
